985c4d7647b26e95ec50ac89af63636b2fc17bf4
Ryu Seunglyul <asimryu@gmail.com>
2018-11-14 17:15:25 +0900

    rest with pagination

--- api/api.php
+++ api/api.php
@@ -2,18 +2,27 @@
 $method = $_SERVER['REQUEST_METHOD'];
 $request = isset($_GET['request']) ? $_GET['request'] : "";
 $uri = explode("/", $request);
-$command = isset($uri[0]) ? $uri[0] : "";
+$api = isset($uri[0]) ? $uri[0] : "";
 $id = isset($uri[1]) ? $uri[1] : 0;
 $dbh = new PDO("mysql:host=localhost; dbname=restful; charset=utf8","root","");
-if( $command == "friend" ) {
-	if( $method == "GET" ) {
+$listperpage = 10;
+if( $api == "friend" ) 
+{
+	if( $method == "GET" ) 
+	{
+		$page = isset($_GET['page']) ? intval($_GET['page']) : 1;
+		if( ! $page ) $page = 1;
+		$start = ($page - 1) * $listperpage;		
 		$sql = "SELECT * FROM friends";
 		if( $id ) $sql .= " WHERE id={$id}";
+		if( ! $id ) $sql .= " LIMIT {$start}, {$listperpage}";		
 		if( $sth = $dbh->query($sql) ) {
 			$friends = $sth->fetchAll(PDO::FETCH_ASSOC);
 			echo json_encode($friends);
 		}
-	} elseif( $method == "DELETE" ) {
+	} 
+	elseif( $method == "DELETE" ) 
+	{
 		$count = 0;
 		if( $id ) {
 			$sql = "DELETE FROM friends WHERE id={$id}";
@@ -22,7 +31,9 @@ if( $command == "friend" ) {
 			}
 		}
 		echo $count;
-	} elseif( $method == "PUT" ) {
+	} 
+	elseif( $method == "PUT" ) 
+	{
 		$count = 0;
 		if( $id ) {
 			$putData = file_get_contents("php://input");
@@ -36,7 +47,9 @@ if( $command == "friend" ) {
 			}
 		}
 		echo $count;
-	} elseif( $method == "POST" ) {
+	} 
+	elseif( $method == "POST" ) 
+	{
 		$count = 0;
 		$name = isset($_POST['name']) ? $_POST['name'] : "";
 		$city = isset($_POST['city']) ? $_POST['city'] : "";
@@ -50,4 +63,23 @@ if( $command == "friend" ) {
 		}
 		echo $count;
 	}
+}  
+elseif( $api == "pages" ) 
+{
+	
+	if( $method == "GET" ) {
+		$totalrows = 0;
+		$totalpages = 0;
+		$sql = "SELECT count(*) as cnt FROM friends";		
+		if( $res = $dbh->query($sql) ) {
+			$data = $res->fetch();
+			$totalrows = $data['cnt'];
+		}
+		if( $totalrows ) {
+			$totalpages = ceil($totalrows/$listperpage);
+		}
+		$result = array("totalrows"=>$totalrows, "totalpages"=>$totalpages);
+		echo json_encode($result);
+	}
+
 }
